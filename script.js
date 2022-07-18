26cf2c6928712debf08059cecc35c2ced699e035
Avinashdaroga15 <19embit013@mlvti.ac.in>
2022-07-18 17:34:14 +0530

    same click ka bug

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-
-//function to suffle array elements
-    function shuffleArray(array) {
-        for (var i = array.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-    }      
-//function for making divs with setting smile image
-    function intialImage()
-    {   
-        for(let i=0;i<16;i++){
-            let div1 = document.createElement("div");
-            // div1.setAttribute("class","img");
-            let img = document.createElement("img");
-            img.setAttribute("id","img"+(i+1));
-            img.setAttribute("src", smileImg);
-            div1.append(img);
-            div.append(div1);
-        }
-    }
-
 //Array of contain images 
     let game_images = ['./img/images.png', './img/html1.png', './img/css.jpeg', './img/C_Programming_Language.svg.png', './img/bootstrap-stack.png', './img/JavaScript-Logo.png',
         './img/jquery.png', './img/python-logo.png']
@@ -65,6 +41,30 @@
         let img = "img"+(i+1);
         arrItm[i]= document.getElementById(img)
     }
+
+//function to suffle array elements
+    function shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }      
+//function for making divs with setting smile image
+    function intialImage()
+    {   
+        for(let i=0;i<16;i++){
+            let div1 = document.createElement("div");
+            // div1.setAttribute("class","img");
+            let img = document.createElement("img");
+            img.setAttribute("id","img"+(i+1));
+            img.setAttribute("src", smileImg);
+            div1.append(img);
+            div.append(div1);
+        }
+    }
+
             
 //function for item event listeners      
     function fun(item, smil, img) { 
@@ -78,30 +78,31 @@
                 previousEventName = item;
             }
         }else{
-            if ("."+item.src.slice(21) == smil) {
+            if ("."+item.src.slice(21) === smil) {
                 item.setAttribute('src', img);
                 previousEvent = 1;
                 Move++;
-            }
-            setTimeout(function () {
-                if ("."+item.src.slice(21) == previousEvSrc) {
-                    previousEventName.removeEventListener('click', fun);
-                    item.removeEventListener('click', fun);
-                    success++;
-                    previousEvent = 1;
-                    previousEventName = " ";
-                    moveDiv.textContent = "Move : " + Move;
-                    successfulMove.textContent = "Successful Moves : " + success;
-                    if (success == 8) {
-                        alert("Great You have won game");
+                console.log("same hua")
+                setTimeout(function () {
+                    if ("."+item.src.slice(21) == previousEvSrc) {
+                        previousEventName.removeEventListener('click', fun);
+                        item.removeEventListener('click', fun);
+                        success++;
+                        previousEvent = 1;
+                        previousEventName = " ";
+                        moveDiv.textContent = "Move : " + Move;
+                        successfulMove.textContent = "Successful Moves : " + success;
+                        if (success == 8) {
+                            alert("Great You have won game");
+                        }
+                    } else {
+                        item.setAttribute('src', smil)
+                        previousEventName.setAttribute('src', smil);
+                        previousEventName = " ";
+                        moveDiv.textContent = "Move : " + Move;
                     }
-                } else {
-                    item.setAttribute('src', smil)
-                    previousEventName.setAttribute('src', smil);
-                    previousEventName = " ";
-                    moveDiv.textContent = "Move : " + Move;
-                }
-            }, 400);
+                }, 400);
+            }
         }
     }
         
